8339a32b06b04d1acc58194fbb6fdb014432131d
caffeine108 <kamila.wyroslak@gmail.com>
2023-10-02 20:18:33 +0200

    feat: make dashboard Dynamic

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,6 +1,47 @@
-import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { BookIcon } from '../Components/Icons/BookIcon';
+import { MessagesIcon } from '../Components/Icons/MessagesIcon';
+import { CalendarIcon } from '../Components/Icons/CalendarIcon';
+
+const dashboardCards = [
+  {
+    title: 'Moje CV',
+    icon: <PersonalCardIcon />,
+    description: 'podgląd CV wraz z doświadczeniem',
+    link: '/cv',
+  },
+
+  {
+    title: 'Ćwiczenia',
+    icon: <EditIcon />,
+    description: 'wszystkie wykonane ćwiczenia',
+    link: '/exercises',
+  },
+
+  {
+    title: 'Blog',
+    icon: <BookIcon />,
+    description: 'wpisy blogowe o technologii front-end',
+    link: '/blog',
+  },
+
+  {
+    title: 'Tech Stack',
+    icon: <CalendarIcon />,
+    description: 'stack technologiczny realizowany na kursie',
+    link: '/blocks',
+  },
+
+  {
+    title: 'FAQ',
+    icon: <MessagesIcon />,
+    description: 'odpowiedzi na najczęstrze pytania',
+    link: '/faq',
+  },
+];
 
 export const Dashboard = () => {
   return (
@@ -10,12 +51,16 @@ export const Dashboard = () => {
         Poniżej znajdziesz najważniejsze informacje na temat mojej działalności.
       </p>
       <div className="dashboard-cards">
-        <div className="dashboard-card">
-          <h3>Moje CV</h3>
-          <PersonalCardIcon />
-          <p>podgląd cv wraz z doświadczeniem</p>
-          <Link to="/cv">zobacz więcej {'>'}</Link>
-        </div>
+        {dashboardCards.map((card) => {
+          return (
+            <div className="dashboard-card">
+              <h3>{card.title}</h3>
+              {card.icon}
+              <p>{card.description}</p>
+              <Link to={card.link}>zobacz więcej {`>`} </Link>
+            </div>
+          );
+        })}
       </div>
       <aside className="dashboard-aside"></aside>
     </div>
