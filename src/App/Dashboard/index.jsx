2ad48d24562754cf2037abd546c206d0c295b9f1
caffeine108 <kamila.wyroslak@gmail.com>
2023-10-02 19:17:32 +0200

    feat: dashborad view one card without map

--- src/App/Dashboard/index.jsx
+++ src/App/Dashboard/index.jsx
@@ -1,13 +1,23 @@
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
   return (
     <div className="dashboard">
-      <h1>Dashboard</h1>
-      <p>
-        Strona główna kursanta (o sobie, zdjęcie, opis narzędzi wykorzystywanych
-        na kursie oraz umejętności zdobytych na kursie)
+      <h1 className="dashboard-greeting">Hej tu Kamila</h1>
+      <p className="dashborad-description">
+        Poniżej znajdziesz najważniejsze informacje na temat mojej działalności.
       </p>
+      <div className="dashboard-cards">
+        <div className="dashboard-card">
+          <h3>Moje CV</h3>
+          <PersonalCardIcon />
+          <p>podgląd cv wraz z doświadczeniem</p>
+          <Link to="/cv">zobacz więcej {'>'}</Link>
+        </div>
+      </div>
+      <aside className="dashboard-aside"></aside>
     </div>
   );
 };
