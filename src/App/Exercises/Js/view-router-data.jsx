568b6023bac861ea63e7cea56aa5617f6cebd027
caffeine108 <kamila.wyroslak@gmail.com>
2023-10-09 21:14:18 +0200

    exercise js arrays basic and loop component added

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -5,6 +5,7 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { emptyValuesAndStringsMetaData } from './EmptyVaulesAndStrings/router-data';
 import { stringMethodsMetaData } from './StringMethods/router-data';
+import { exerciseJsArrayBasicsMetaData } from './Exercise-js-arrays-basics/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
@@ -12,5 +13,6 @@ export const blockRouterMetaData = [
   blockRouterMetaDataJsFunction1,
   emptyValuesAndStringsMetaData,
   stringMethodsMetaData,
+  exerciseJsArrayBasicsMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
