36274344ea63d2bac408b03275634b4b96b236a9
caffeine108 <kamila.wyroslak@gmail.com>
2023-11-13 20:57:27 +0100

    forebase exercise

---
+++ src/App/Exercises/ReactExercises/ReactFirebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDh03GG3NcAlcc917Mh4e0TZz-g-Oz2CXQ',
+  authDomain: 'pomeranian-bolilerplate.firebaseapp.com',
+  projectId: 'pomeranian-bolilerplate',
+  storageBucket: 'pomeranian-bolilerplate.appspot.com',
+  messagingSenderId: '1083227487412',
+  appId: '1:1083227487412:web:a889124437e72ccff1abe1',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
